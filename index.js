52e142872fe33b4ea3d781d702cebac117eb0d55
The_Pr0fessor <Trendonrobinson@gmail.com>
2022-03-04 13:34:05 -0500

    BackendFinished

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose')
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
-const Post = require('./models/Post')
 const { MONGODB } = require('./config')
+
+// const pubsub = new PubSub();
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: ({ req }) => ({ req })
 });
 
 mongoose
